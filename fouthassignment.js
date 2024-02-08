14562c4c29261fa0fdea40be22f97b960346f0d5
Maruf Hossain <mdmarufh974@gmail.com>
2024-02-08 22:13:54 +0600

    problem 2 solve

--- fouthassignment.js
+++ fouthassignment.js
@@ -1,3 +1,4 @@
+// Problem 1
 function calculateMoney(ticketSale) {
     if (0 >= ticketSale){
         const message = "Please Input Valid Number"
@@ -16,3 +17,28 @@ calculateMoney(10);
 calculateMoney(1055);
 calculateMoney(93);
 calculateMoney(0);
+
+// Problem 2
+
+function checkName(name) {
+    if (typeof name !== 'string') {
+        const errorMessage = "invalid";
+        return errorMessage;
+    };
+    const lastLetter = name.slice(-1).toLowerCase();
+    
+    const lastLetters = ['y', 'i', 'e', 'o', 'u', 'w'];
+    if (lastLetters.includes(lastLetter)) {
+        const message = "Good Name";
+        return message;
+    }
+    else {
+        const message = "Bad Name";
+        return message;
+    }
+}
+checkName("Salman");
+checkName("RAFEE")
+checkName("Jhankar")
+checkName(199)
+checkName(["Rashed"])
